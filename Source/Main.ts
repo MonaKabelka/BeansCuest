fd883572d9c470ff629ca483dfa60f5bd464e16b
MonaKabelka <mona.kabelka.mk@gmail.com>
2023-07-20 17:42:04 +0200

    build

--- Source/Main.ts
+++ Source/Main.ts
@@ -36,12 +36,12 @@ namespace BeansCuest {
     gameMenu = fS.Menu.create(transformMenu(menuDefinition), useCallbacks, "in-game-menu");
     gameMenu.close();
     let scenes: fS.Scenes = [
-      // { scene: scene1_1, name: "Scene 1.1" },
-      // { scene: scene1_2, name: "Scene 1.2" },
-      // { scene: scene2_1, name: "Scene 2.1" },
-      // { scene: scene3_1, name: "Scene 3.1" },
-      // // { scene: scene4_1, name: "Scene 4.1" },
-      { scene: scene5, name: "Scene 5"}
+      { scene: scene1_1, name: "Scene 1.1" },
+      { scene: scene1_2, name: "Scene 1.2" },
+      { scene: scene2_1, name: "Scene 2.1" },
+      { scene: scene3_1, name: "Scene 3.1" },
+      { scene: scene4_1, name: "Scene 4.1" },
+      { scene: scene5, name: "Scene 5"},
       { scene: scene6, name: "Scene 6"}
     ];
 
